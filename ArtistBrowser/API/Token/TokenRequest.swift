10b6bb191fb64f6724ce2bf54039c76b66728eaf
Lukas Bahrle <lb.github.test@gmail.com>
2021-02-24 21:48:38 +0100

    create TokenRequest

---
+++ ArtistBrowser/API/Token/TokenRequest.swift
@@ -0,0 +1,31 @@
+//
+//  TokenRequest.swift
+//  ArtistBrowser
+//
+//  Created by Lukas Bahrle Santana on 24/02/2021.
+//
+
+import Foundation
+
+struct TokenRequest: Request {
+    typealias CredentialsLoader = () -> Credentials
+    
+    let builder: RequestBuilder
+    let credentialsLoader: CredentialsLoader
+    
+    init(builder: RequestBuilder, credentialsLoader: @escaping CredentialsLoader){
+        self.builder = builder
+        self.credentialsLoader = credentialsLoader
+    }
+    
+    func get() -> URLRequest {
+        let credentials = credentialsLoader()
+        
+        var request = builder.build()
+        let authHeader = URLRequest.basicBase64EncodedAuthorizationHeader(username: credentials.username, password: credentials.password)
+        request.addValue(authHeader.value, forHTTPHeaderField: authHeader.key)
+       
+        return request
+    }
+    
+}
