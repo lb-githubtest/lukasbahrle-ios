ebe3e3ddaedb7142a8d6cea4dece2a92480d443a
Lukas Bahrle <lb.github.test@gmail.com>
2021-02-24 18:08:25 +0100

    create Request and RequestBuilder interfaces to build the URLRequests

--- ArtistBrowserTests/LoadTokenFromRemoteTests.swift
+++ ArtistBrowserTests/LoadTokenFromRemoteTests.swift
@@ -8,6 +8,106 @@
 import XCTest
 import ArtistBrowser
 
+
+enum HTTPMethod: String{
+    case GET = "GET"
+    case POST = "POST"
+}
+
+
+
+
+
+
+protocol RequestBuilder{
+    var baseURL: URL { get }
+    var path: String { get }
+    var params: [URLQueryItem]? { get }
+    var headers: [String: String]? {get}
+    var httpMethod: HTTPMethod {get}
+    var body: Data {get}
+    
+    func build() -> URLRequest
+}
+
+extension RequestBuilder{
+    var headers: [String: String]? {
+        nil
+    }
+    
+    var httpMethod: HTTPMethod{
+        HTTPMethod.GET
+    }
+    
+    var body: Data {
+        Data()
+    }
+    
+    var params: [URLQueryItem]? {
+        nil
+    }
+    
+    func build() -> URLRequest {
+        guard var components = URLComponents(url: baseURL.appendingPathComponent(path), resolvingAgainstBaseURL: false) else {
+            fatalError("URL path error")
+        }
+        components.queryItems = params
+        
+        guard let url = components.url else {
+            fatalError("URL path error")
+        }
+        
+        var request = URLRequest(url: url)
+        
+        if let headers = headers {
+            for (key, value) in headers {
+                request.addValue(value, forHTTPHeaderField: key)
+            }
+        }
+        request.httpBody = body
+        request.httpMethod = httpMethod.rawValue
+        return request
+    }
+}
+
+
+protocol Request {
+    var builder: RequestBuilder { get }
+    func get() -> URLRequest
+}
+
+extension Request {
+    func get() -> URLRequest {
+        builder.build()
+    }
+}
+
+
+struct TokenRequest: Request {
+    typealias CredentialsLoader = () -> Credentials
+    
+    let builder: RequestBuilder
+    let credentialsLoader: CredentialsLoader
+    
+    init(builder: RequestBuilder, credentialsLoader: @escaping CredentialsLoader){
+        self.builder = builder
+        self.credentialsLoader = credentialsLoader
+    }
+    
+    func get() -> URLRequest {
+        let credentials = credentialsLoader()
+        
+        var request = builder.build()
+        let authHeader = URLRequest.basicBase64EncodedAuthorizationHeader(username: credentials.username, password: credentials.password)
+        request.addValue(authHeader.value, forHTTPHeaderField: authHeader.key)
+       
+        return request
+    }
+    
+}
+
+
+
 class HTTPClientSpy: HTTPClient {
     private var messages = [(request: URLRequest, completion: (HTTPClient.Result) -> Void)]()
     
@@ -46,22 +146,32 @@ public protocol TokenLoader{
     func load(completion: @escaping (Result) -> Void)
 }
 
+public struct Credentials{
+    public let username: String
+    public let password: String
+    
+    public init(username: String, password: String){
+        self.username = username
+        self.password = password
+    }
+}
+
 class RemoteTokenLoader: TokenLoader{
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
     }
     
-    let url: URL
+    let request: Request
     let client: HTTPClient
     
-    init(url: URL, client: HTTPClient){
-        self.url = url
+    init(request: Request, client: HTTPClient){
+        self.request = request
         self.client = client
     }
     
     func load(completion: @escaping (TokenLoader.Result) -> Void) {
-        client.get(request: request()) { result in
+        client.get(request: request.get()) { result in
             switch result{
             case let .success((data, httpResponse)):
                 completion(self.map(data, from: httpResponse))
@@ -71,10 +181,6 @@ class RemoteTokenLoader: TokenLoader{
         }
     }
     
-    private func request() -> URLRequest{
-        let request = URLRequest(url: url)
-        return request
-    }
     
     private func map(_ data: Data, from response: HTTPURLResponse) -> TokenLoader.Result {
         do {
@@ -113,6 +219,16 @@ class LoadTokenFromRemoteTests: XCTestCase {
         XCTAssertTrue(client.requests.isEmpty)
     }
     
+    func test_load_requestDataFromURL(){
+        let builder = BasicRequestBuilder(baseURL: URL(string: "https://test")!, path: "path")
+        let (sut, client) = makeSUT(request: BasicRequest(builder: builder))
+       
+        sut.load{ _ in}
+        
+        XCTAssertEqual(client.requests.count, 1)
+        XCTAssertEqual(client.requests[0].url, URL(string: "https://test/path"))
+    }
+    
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         
@@ -141,8 +257,12 @@ class LoadTokenFromRemoteTests: XCTestCase {
         })
     }
     
+   
+    
     func test_load_deliversResultOn200HTTPResponseWithValidTokenJSON() throws{
-        let (sut, client) = makeSUT()
+        
+        let builder = BasicRequestBuilder(baseURL: URL(string: "https://test")!, path: "path")
+        let (sut, client) = makeSUT(request: BasicRequest(builder: builder))
         
         let token = "anytoken"
         let json: [String: Any] = [
@@ -159,9 +279,9 @@ class LoadTokenFromRemoteTests: XCTestCase {
     
     // MARK: Helpers
     
-    private func makeSUT(url: URL = URL.any()) -> (RemoteTokenLoader, HTTPClientSpy){
+    private func makeSUT(request: Request = BasicRequest.any()) -> (RemoteTokenLoader, HTTPClientSpy){
         let client = HTTPClientSpy()
-        let sut = RemoteTokenLoader(url: url, client: client)
+        let sut = RemoteTokenLoader(request: request, client: client)
         
         return (sut, client)
     }
@@ -232,3 +352,43 @@ public extension HTTPURLResponse{
         self.statusCode == 200
     }
 }
+
+
+extension Credentials{
+    static func any() -> Credentials{
+        Credentials(username: "username", password: "password")
+    }
+}
+
+
+
+extension URLRequest {
+    static func basicBase64EncodedAuthorizationHeader(username: String, password: String) -> (key: String, value: String){
+        let encodedCredentials = Data("\(username):\(password)".utf8).base64EncodedString()
+        return ("Authorization", "Basic \(encodedCredentials)")
+    }
+}
+
+
+
+
+
+struct BasicRequestBuilder: RequestBuilder{
+    var baseURL: URL
+    var path: String
+    
+    static func any() -> BasicRequestBuilder{
+        return BasicRequestBuilder(baseURL: URL.any(), path: "")
+    }
+    
+}
+
+struct BasicRequest: Request{
+    var builder: RequestBuilder
+    
+    static func any() -> Request{
+        BasicRequest(builder: BasicRequestBuilder.any())
+    }
+}
+
+
