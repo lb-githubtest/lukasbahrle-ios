01635a54210edb8a663920f70ab620f7e406bf4f
Lukas Bahrle <lb.github.test@gmail.com>
2021-02-24 14:05:11 +0100

    add ArtistBrowser Framework to separate UIKit from UIKit independent modules

---
+++ ArtistBrowser/ArtistBrowser.h
@@ -0,0 +1,18 @@
+//
+//  ArtistBrowser.h
+//  ArtistBrowser
+//
+//  Created by Lukas Bahrle Santana on 24/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArtistBrowser.
+FOUNDATION_EXPORT double ArtistBrowserVersionNumber;
+
+//! Project version string for ArtistBrowser.
+FOUNDATION_EXPORT const unsigned char ArtistBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArtistBrowser/PublicHeader.h>
+
+
